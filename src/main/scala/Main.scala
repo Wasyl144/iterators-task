6b3d7d2349ad200d2173bc31a48f2c1b64425cc1
Michal Frankiewicz <wasyl144@outlook.com>
2022-06-09 00:19:53 +0200

    Test streams

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import classes.{AppConfig, Application}
 import com.typesafe.scalalogging.Logger
 import pureconfig._
@@ -9,10 +11,6 @@ object Main extends App {
   val logger: Logger = Logger(getClass.getName)
   val config: AppConfig = ConfigSource.default.at("app").loadOrThrow[AppConfig]
   val filePath: Option[String] = FileUtils.validateArgs(args.toList)
-
-  println(filePath)
-  logger.debug("test")
-
 
   val app = new Application(config = config, filePath)
   app.run()
--- src/main/scala/classes/Application.scala
+++ src/main/scala/classes/Application.scala
@@ -1,11 +1,31 @@
 package classes
 
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, IOResult}
 import com.typesafe.scalalogging.Logger
+import akka.stream.scaladsl._
+
+import java.nio.file.Paths
+import scala.concurrent.Future
 
 class Application(val config: AppConfig, val filePath: Option[String]) {
   def run(): Unit = {
     val logger: Logger = Logger[Application]
     logger.debug("test debugowy z klasy application")
+    // Init akka.actors
+    implicit val system: ActorSystem = ActorSystem("Task")
 
+    // Init Akka streams
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    val fileStream = filePath match {
+      case Some(x) => Some(Paths.get(x))
+      case None => None
+    }
+//    val file = Paths.get("example.csv")
+
+    val foreach: Option[Future[IOResult]]= fileStream match {
+      case Some(value) => Some(FileIO.fromPath(value).to(Sink.ignore).run())
+      case None => None
+    }
   }
 }
